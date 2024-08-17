4ad4ba7297232d5171eb2a54e0fb1e6c7fcb18f8
Artur Skubei <41114720+gdlbo@users.noreply.github.com>
2024-08-17 16:13:55 +0300

    bump to 1.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "ru.parcel.app"
         minSdk = 22
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
 
         vectorDrawables {
             useSupportLibrary = true
