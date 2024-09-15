f8d6a7e5dbc1f4bf9e99ef8fd2b2c4d697202247
Artur Skubei <41114720+gdlbo@users.noreply.github.com>
2024-09-15 17:19:44 +0300

    Replace logo with unofficial version

--- app/src/main/java/ru/gdlbo/parcelradar/app/ui/components/Logo.kt
+++ app/src/main/java/ru/gdlbo/parcelradar/app/ui/components/Logo.kt
@@ -3,28 +3,25 @@ package ru.gdlbo.parcelradar.app.ui.components
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import ru.gdlbo.parcelradar.app.R
 
-import java.util.*
-
 @Composable
 fun AppLogo() {
-    val logoId = if (Locale.getDefault().language != "ru") {
-        R.drawable.logo_gp_en
-    } else {
-        R.drawable.logo_gp
-    }
-
     Image(
-        painter = painterResource(id = logoId),
+        painter = painterResource(id = R.drawable.ic_logo_unofficial),
         contentDescription = stringResource(R.string.app_name),
         modifier = Modifier
-            .size(width = 300.dp, height = 100.dp)
+            .size(width = 175.dp, height = 200.dp)
             .padding(bottom = 32.dp)
+            .clip(RoundedCornerShape(16.dp)),
+        contentScale = ContentScale.Crop
     )
 }
